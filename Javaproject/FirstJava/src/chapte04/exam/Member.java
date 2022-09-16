254ab0503339742e5790cf7c40587286357fe1bd
김주환 <juhwan71047@gmail.com>
2022-09-16 17:31:19 +0900

    Member2

--- Javaproject/FirstJava/src/chapte04/exam/Member.java
+++ Javaproject/FirstJava/src/chapte04/exam/Member.java
@@ -3,18 +3,16 @@ package chapte04.exam;
 import java.util.Calendar;
 import java.util.Scanner;//10번 라인에 Scanner를 사용하기 위해서 사용됨
 
-import chapte03.Calculator;
-
 public class Member {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); // sc라는 이름으로 사용자에 입력을 받 스캐너를 가져옴
+	System.out.print("");
 		while (true) {
-
 			System.out.print("생년를 입력하세요 : ");
-			Scanner sc = new Scanner(System.in); // sc라는 이름으로 사용자에 입력을 받 스캐너를 가져옴
 			int age = sc.nextInt();// 사용자에 입력을 int로 받아서 age 변수에 넣음
 			flu(age);// 19번라인 메소드로 이동함
-			birthday(age);// 사용자에게서 입력받아 SimpleDateFormat변수에 넣은 값을 29번 줄 메소드로 넘겨줌
+			birthday(age);// 사용자에게서 입력받아 age변수에 넣은 값을 birthday()로 넘겨줌
 			System.out.println("-----------------------");
 		}
 
@@ -33,18 +31,18 @@ public class Member {
 
 	}
 
-	public static int birthday(int age) {// int SimpleDateFormat라는 매개변수를 가지고 있음
+	public static int birthday(int age) {// int age라는 매개변수를 가지고 있음
 		int currYear = Calendar.getInstance().get(Calendar.YEAR);
-		int reBirth = currYear - age;
+		int reBirth = currYear - age + 1;
 		if (reBirth < 20) {// 이번년도 - 태어난년도 차를 구해서 20보다 작으면 아래코드 실행
-			System.out.println(reBirth + "미성년자 입니다.");
-		} else {// 31번줄 if 값이 false면 34번줄 else문이 실행됨
-			if (reBirth < 15 || reBirth > 65) {// 입력년도를 2로나눈 나머지를 구하고, 올해를 2로나눈 나머지를 구해서 서로 같은지 비교함
-				System.out.println(age + " 내년에 무료건강검진이 가능합니다.");// 입력년도랑 올해년도가 둘다 짝수면 실행
-			} else {// 35번줄 값이 false면 37번 줄 실행
-				System.out.println(age + " 올해 무료건강검진이 가능합니다.");// 사용자가 입력한 값 표출 + 내용
+			System.out.println(reBirth + "세 미성년자 입니다.");
+		} else {
+			if (reBirth % 2 == 1 || age % 2 == 0) {
+				System.out.println("내년에 무료건강검진이 가능합니다.");// 입력년도랑 올해년도가 둘다 짝수면 실행
+			} else {
+				System.out.println("올해 무료건강검진이 가능합니다.");// 사용자가 입력한 값 표출 + 내용
 			}
-			if (reBirth >= 40) {// 올해 - 사용자입력년도가 40보다 크거나 같으면 41번 라인 실행 됨
+			if (reBirth >= 40) {
 				System.out.println("연세 " + (reBirth) + "세 암 검사도 무료로 가능합니다.");
 			}
 
