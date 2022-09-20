68776d218b69b1c2791501889e6717d3d60880e7
김주환 <juhwan71047@gmail.com>
2022-09-20 16:30:00 +0900

    ContactMain_if

--- Javaproject/OOPMiniProject/src/ver01/ContactMain.java
+++ Javaproject/OOPMiniProject/src/ver01/ContactMain.java
@@ -12,9 +12,15 @@ public class ContactMain {
 		String address;// 주소
 		int birthday;// 생일
 		String group;// 그룹
+
+		String no;
 		while (true) {
 			System.out.print("이름을 입력해 주세요-->");
 			name = sc.nextLine();
+			if ("no".equalsIgnoreCase(name)) {
+				System.out.println("종료");
+				break;
+			}
 			System.out.print("전화번호를 입력해 주세요-->");
 			phonenumber = sc.nextInt();
 			sc.nextLine();
@@ -31,6 +37,7 @@ public class ContactMain {
 			Contact con = new Contact(name, phonenumber, email, address, birthday, group);
 
 			con.showContactInfo();
+			System.out.println("계속 하시려면 이름을 입력해주시고 그만하시려면 No를 입력해 주세요");
 		}
 	}
 
