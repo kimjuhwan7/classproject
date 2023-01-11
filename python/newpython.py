1018d39f02f3dba5d812d773c39b436a897c5606
김주환 <juhwan71047@gmail.com>
2023-01-11 14:34:15 +0900

    포털뉴스크롤링데이터분석.html

---
+++ python/newpython.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+a=250
+
+
+# In[20]:
+
+
+print(a)
+
+
+# In[29]:
+
+
+a
+
+
+# In[25]:
+
+
+print(a)
+
+
+# In[26]:
+
+
+a=300
+
+
+# In[27]:
+
+
+print(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
